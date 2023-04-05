2ba874e003a05f66e077be9ab8382e2e5413b2cb
AVcemik <aveal.cemik@gmail.com>
2023-04-05 19:08:17 +0200

    _1_Work - HomeWork (Working)

---
+++ 21-HomeWork 03_04_2023/_1_Work/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Work
+{
+    public static class Command
+    {
+        public const string write = "запись";
+        public const string read = "чтение";
+    }
+}
--- 21-HomeWork 03_04_2023/_1_Work/_1_Work.cs
+++ 21-HomeWork 03_04_2023/_1_Work/_1_Work.cs
@@ -7,25 +7,74 @@ namespace _1_Work
     {
         static void Main(string[] args)
         {
+                //File.WriteAllText(_fullPath, "");
+                //File.Create(_fileName);
             string _directory = "c:\\Docs";
             string _fileName = "Data.txt";
             string _fullPath = "";
-            List<string> _data = new List<string>();
-
-
             _fullPath = Path.Combine( _directory, _fileName );
-            if (Directory.Exists("c:\\Docs") != true)
-            {
-                Directory.CreateDirectory(_directory);
-                File.Create(_fileName);
-            }
-            else if (Directory.Exists("c:\\Docs\\data.txt") != true)
-            {
-                File.Create(_fileName);
-            }
-            else
+            List<string> _content = new List<string>();
 
-            Console.ReadKey();
+            CheckDir(_directory);
+
+
+            switch (RunCommand())
+            {
+                case Command.write:
+                    WriteFile(_fullPath);
+                    break;
+
+                case Command.read:
+                    ReadFile(_fullPath);
+                    break;
+            }
+
+
+            void CheckDir(string directory)
+            {
+                if (Directory.Exists("c:\\Docs") != true)
+                {
+                    Directory.CreateDirectory(directory);
+                }
+            }
+            string RunCommand()
+            {
+                Console.Write("Введите комманду (запись/чтение): ");
+                string command = Console.ReadLine();
+                return command.ToLower().Trim();
+            }
+            void WriteFile(string fullPath)
+            {
+                List<string> content = File.ReadAllLines(fullPath).ToList();
+
+                Console.Write("Введите Заголовок: ");
+                string title = Console.ReadLine().ToUpper();
+
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+
+                string time = DateTime.Now.ToString();
+                content.Add(title + " | " + text + " | " + time);
+
+                File.WriteAllLines(_fullPath, content);
+            }
+            string[] ReadFile(string fullPath)
+            {
+                Console.WriteLine("");
+                string[] text = File.ReadAllLines(fullPath);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    Console.WriteLine(text[i]);
+                }
+                return text;
+            }
+
+            void ExitProgramm()
+            {
+                Console.WriteLine("Нажмите любую клавишу для завершения...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
 
         }
     }
