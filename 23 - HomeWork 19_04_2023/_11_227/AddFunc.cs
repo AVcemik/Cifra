5ed5cdc5e2141fc5087806e5401208691324fbc6
AVcemik <aveal.cemik@gmail.com>
2023-04-20 15:33:53 +0200

    Starting Homework

---
+++ 23 - HomeWork 19_04_2023/_11_227/AddFunc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_227
+{
+    public class AddFunc
+    {
+        static public void Exit()
+        {
+            Console.WriteLine("\n\nНажмите любую клавишу для завершения...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
