8e586d6842d3ad5b139aaa46163f7840336a1ce1
AVcemik <aveal.cemik@gmail.com>
2023-04-21 17:20:53 +0200

    Finished

--- 23 - HomeWork 19_04_2023/_11_227/AddFunc.cs
+++ 23 - HomeWork 19_04_2023/_11_227/AddFunc.cs
@@ -8,6 +8,49 @@ namespace _11_227
 {
     public class AddFunc
     {
+
+        static public void FillMass(double[,] fallout, out int snow, out int rain)
+        {
+            Random r = new Random();
+            snow = 0;
+            rain = 0;
+
+            for (int i = 0; i < fallout.GetLength(0); i++)
+            {
+                for (int j = 0; j < fallout.GetLength(1); j++)
+                {
+                    if (j == 0) fallout[i, j] = i + 1;
+                    else if (j == 1) fallout[i, j] = r.NextDouble() + r.Next(0, 10);
+                    else fallout[i, j] = r.NextDouble() + r.Next(-10, 10);
+                }
+
+                if (fallout[i, 2] <= 0) snow += Convert.ToInt32(fallout[i, 1]);
+                else rain += Convert.ToInt32(fallout[i, 2]);
+            }
+        }
+
+        static public void OutputMass(double[,] fallout)
+        {
+
+
+            Console.WriteLine("День\tОсадки\tТемпература");
+            for (int i = 0; i < fallout.GetLength(0); i++)
+            {
+                for (int j = 0; j < fallout.GetLength(1); j++)
+                {
+                    if (j == 0 && i < 10-1) Console.Write($" {fallout[i, j]:F0}\t");
+                    else if (j == 0) Console.Write($" {fallout[i, j]:F0}\t");
+                    else if (j == 1) Console.Write(($"  {fallout[i, j]:F0}\t"));
+                    else
+                    {
+                        if (fallout[i,j] >=0) Console.Write($"  {fallout[i, j]:F1}\t");
+                        else Console.Write($" {fallout[i, j]:F1}\t");
+                    }
+                }
+
+                Console.WriteLine($"");
+            }
+        }
         static public void Exit()
         {
             Console.WriteLine("\n\nНажмите любую клавишу для завершения...");
--- 23 - HomeWork 19_04_2023/_11_227/_11_227.cs
+++ 23 - HomeWork 19_04_2023/_11_227/_11_227.cs
@@ -8,25 +8,13 @@
 // (Считать, что идет дождь, если температура воздуха выше 0 С.)
 
 
-double[,] fallout = new double[30,2];
-Random r = new Random();
+double[,] _fallout = new double[30,3];
+int _snow, _rain;
 
-for (int i = 0; i < fallout.GetLength(0); i++)
-{
-    for (int j = 0; j < fallout.GetLength(1); j++)
-    {
-        fallout[i, j] = r.NextDouble() + r.Next(-10, 10);
-    }
-}
+AddFunc.FillMass(_fallout, out _snow, out _rain);
+AddFunc.OutputMass(_fallout);
 
-for (int i = 0; i < fallout.GetLength(0); i++)
-{
-    for (int j = 0; j < fallout.GetLength(1); j++)
-    {
-        Console.Write($"{fallout[i,j]:F2} ");
-    }
-    Console.WriteLine($"");
-}
+Console.WriteLine($"\nКол-во снега: {_snow} \nКол-во дождя: {_rain}");
 
 
 
