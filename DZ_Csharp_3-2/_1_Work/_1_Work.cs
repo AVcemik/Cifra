94cdbd31af5068c09b8d77e5d9da2cf751e87838
cemik <aveal.cemik@gmail.com>
2023-04-30 11:41:15 +0200

    new DZ

---
+++ DZ_Csharp_3-2/_1_Work/_1_Work.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Schema;
+
+namespace _1_Work
+{
+    internal class _1_Work
+    {
+        static void Main(string[] args)
+        {
+            // Повторить материал по методичке, используя сочетание не “Hello 
+            // World”, а “Добиться успеха легко, познать смысл - вот проблема”
+
+            string firstWorld = "Добиться успеха легко";
+            string secondWorld = "познать смысл";
+            string thirdWorld = "вот проблема";
+
+            string result = firstWorld + ", " + secondWorld;
+            result = string.Concat(result, " -");
+            result = $"{result} {thirdWorld}";
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
