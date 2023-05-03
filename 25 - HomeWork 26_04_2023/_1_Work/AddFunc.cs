cce558b18b54f264ba26e3ad04b725eb46ac8f35
AVcemik <aveal.cemik@gmail.com>
2023-05-03 11:49:27 +0200

    HomeWork 26_04_2023 12.109

---
+++ 25 - HomeWork 26_04_2023/_1_Work/AddFunc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Work
+{
+    public class AddFunc
+    {
+        public static int[,] Fill2DArray(int x, int y)
+        {
+            Random rnd = new Random();
+            int[,] array = new int[x,y];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1) - 1; j++)
+                {
+                    if (j == 0) array[i, j] = i+1;
+                    else array[i, j] = rnd.Next(10, 100);
+                }
+            }
+            return array;
+        }
+        public static int[,] Input2DArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j == 0) Console.Write(array[i, j] + "\t");
+                    else if (j == array.GetLength(1) - 1) { array[i, 3] = array[i, 1] + array[i, 2]; Console.Write(" = " + array[i, j]); }
+                    else Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            return array;
+        }
+        public static void ExitProgramm()
+        {
+            Console.WriteLine("\n\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ 25 - HomeWork 26_04_2023/_1_Work/_1_Work.cs
@@ -0,0 +1,34 @@
+﻿namespace _1_Work
+{
+    internal class _1_Work
+    {
+        static void Main(string[] args)
+        {
+            // Лубенец - 12.109
+            // Дан двумерный массив из пятнадцати строк и двух столбцов.
+            // Найти номера двух соседних строк, сумма элементов в которых минимальна.
+
+            Random rnd = new Random();
+
+            int _result = -1;
+            int _x = 15;      // Размер массива 0
+            int _y = 4;     // Размер массива 1
+            int[,] _array2D = new int[_x,_y];
+
+            _array2D = AddFunc.Fill2DArray(_x, _y);
+            AddFunc.Input2DArray(_array2D);
+
+            for (int i = 0, result = int.MaxValue; i < _array2D.GetLength(0); i++)
+            {
+                if (result >= _array2D[i,1] + _array2D[i, 2])
+                {
+                    result = _array2D[i, 1] + _array2D[i, 2];
+                    _result = _array2D[i, 0];
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("В строке № " + _result + " минимальная сумма элементов");
+            AddFunc.ExitProgramm();
+        }
+    }
+}
