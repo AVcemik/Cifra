194964d544d38b90afb125d5f29280ce922e6967
AVcemik <aveal.cemik@gmail.com>
2023-06-07 14:14:51 +0200

    newHW

---
+++ 18-1 - HomeCifra/_1_Work/Program.cs
@@ -0,0 +1,38 @@
+﻿// Домашнее задание.
+// Тема 18. Урок 1.
+// Работа со строками. Метод Split.
+
+string[] forbiddenWords = new string[] {"слово" };    // содержащий ключевые слова, которые должны быть скрыты в предложении
+string sentence = "";                            // для хранения введенного предложения
+
+while (true)
+{
+    bool flag = false;
+    Console.Write("Введите слово (Для завершения введите \"exit\"): ");
+    string word = Console.ReadLine()!;
+    word = word.Trim();
+
+    for (int i = 0; i < forbiddenWords.Length; i++)
+    {
+        if (forbiddenWords[i] == word)
+        {
+            sentence += "(скрыто)";
+            flag = true;
+            break;
+        }
+
+        if (word == "exit")
+        {
+            flag = true;
+            break;
+        }
+    }
+    if (flag == true) break;
+    sentence += word + " ";
+}
+
+Console.WriteLine("Ваше составленное предложение: " + sentence);
+
+Console.WriteLine("\n\nНажмите любую клавишу для завершения...");
+Console.ReadKey();
+Environment.Exit(0);
