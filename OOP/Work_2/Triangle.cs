04e8ede16ec6a12e089f66346844e2c355cbf9c5
AVcemik <aveal.cemik@gmail.com>
2023-07-17 15:26:10 +0200

    вHome Work

---
+++ OOP/Work_2/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_2
+{
+    internal class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double getA()
+        {
+            return a;
+        }
+
+        public double getB()
+        {
+            return b;
+        }
+
+        public double getC()
+        {
+            return c;
+        }
+
+        public void setA(double ab)
+        {
+            if (ab > 0) a = ab;
+        }
+        public void setB(double bc)
+        {
+            if (bc > 0) b = bc;
+        }
+        public void setC(double ca)
+        {
+            if (ca > 0) c = ca;
+        }
+        public double getArea()
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        public double getPerimetr()
+        {
+            if (a != 0 && b != 0 && c != 0) return a + b +c;
+            return 0;
+        }
+        public double getMedian()
+        {
+
+        }
+    }
+}
---
+++ OOP/Work_2/Work_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using Work_2;
+// Домашняя работа (Work_1, Work_2)
+
+// 2.	Описать класс, представляющий треугольник.
+// Предусмотреть методы для создания объектов, вычисления площади,
+// периметра и точки пересечения медиан. Описать методы для
+// получения состояния объекта.
+
+
+Triangle triang = new Triangle();
+
+Console.Write("Введите сторону А: ");
+triang.setA(double.Parse(Console.ReadLine()!));
+
+Console.Write("Введите сторону В: ");
+triang.setB(double.Parse(Console.ReadLine()!));
+
+Console.Write("Введите сторону С: ");
+triang.setC(double.Parse(Console.ReadLine()!));
+
+
+Console.WriteLine();
+Console.WriteLine($"Сторона А: {triang.getA()} Сторона В: {triang.getB()} Сторона С: {triang.getC()}");
+Console.WriteLine($"Площадь: {triang.getArea():F2}");
+Console.WriteLine($"Периметр: {triang.getPerimetr():F2}");
