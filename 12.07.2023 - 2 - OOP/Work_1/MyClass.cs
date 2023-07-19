91d818a652f704a7bfb2fd66de1f50ae017ea2ca
AVcemik <aveal.cemik@gmail.com>
2023-07-19 12:25:37 +0200

    new Class Mas Work_1

---
+++ 12.07.2023 - 2 - OOP/Work_1/MyClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._07._2023___2___OOP
+{
+    internal class MyClass
+    {
+        private int[] mas;
+        
+        public MyClass(int size) 
+        {
+            mas = new int[size];
+        }
+
+        public void FillRandom()
+        {
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(20);
+            }
+        }
+        public void Shuffle()
+        {
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int j = r.Next(mas.Length);
+                int temp = mas[i];
+                mas[i] = mas[j];
+                mas[j] = temp;
+            }
+        }
+        public void CountElement()
+        {
+
+        }
+
+        public void InputMas()
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"{mas[i]} ");
+            }
+            Console.WriteLine();
+        }
+        ~MyClass()
+        {
+            mas = new int[0];
+        }
+    }
+}
