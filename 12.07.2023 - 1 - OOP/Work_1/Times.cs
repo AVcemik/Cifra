c786460640fef3e007de530c9bc95a731eff087c
AVcemik <aveal.cemik@gmail.com>
2023-07-19 09:56:52 +0200

    ready

---
+++ 12.07.2023 - 1 - OOP/Work_1/Times.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._07._2023___1___HomeCifra
+{
+    internal class Times
+    {
+        private byte h;
+        private byte m;
+        private byte s;
+        private byte menu;
+
+        public Times()
+        {
+            this.h = 0;
+            this.m = 0;
+            this.s = 0;
+            this.menu = 0;
+        }
+
+        public byte Hours
+        {
+            get => h;
+            set
+            {
+                if (value >= 0 && value <= 24) h = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Вышли за диапозон часов (0-24)");
+                }
+            }
+        }
+        public byte Minutes
+        {
+            get => m;
+            set
+            {
+                if (value >=0 && value <= 59) m = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Вышли за диапозон минут (0-59)");
+                }
+            }
+        }
+        public byte Secundes
+        {
+            get => s;
+            set
+            {
+                if (value >=0 && value <= 59) s = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Вышли за диапозон секунд (0-59)");
+                }
+            }
+        }
+        public byte Menu
+        {
+            get => menu;
+            set
+            {
+                if (value >= 1 && value <= 5) menu = value;
+                else if (value == 255) menu = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Вышли за диапозон меню (1-5)");
+                }
+            }
+        }
+        public void setHours(byte h) 
+        {
+            Hours = h;
+        }
+        public void setMinutes(byte m)
+        {
+            Minutes = m;
+        }
+        public void setSecundes(byte s) 
+        {
+            Secundes = s;
+        }
+        public void setTimes(byte h, byte m, byte s)
+        {
+            Hours = h;
+            Minutes = m;
+            Secundes = s;
+        }
+        public string getTimes()
+        {
+            return $"{Hours}:{Minutes}:{Secundes}";
+        }
+
+    }
+}
--- 12.07.2023 - 1 - OOP/Work_1/Work_1.cs
+++ 12.07.2023 - 1 - OOP/Work_1/Work_1.cs
@@ -8,10 +8,59 @@
 // Создать методы изменения времени на заданное количество
 // часов, минут и секунд.
 
-// 2.	Описать класс, представляющий треугольник.
-// Предусмотреть методы для создания объектов, вычисления площади,
-// периметра и точки пересечения медиан. Описать методы для
-// получения состояния объекта.
+using _12._07._2023___1___HomeCifra;
+
+Times t = new();
+
+while (true)
+{
+    Console.WriteLine($"Время: {t.getTimes()}");
+    Console.WriteLine();
+    Console.WriteLine("Меню: ");
+    Console.WriteLine("1 - Установить время");
+    Console.WriteLine("2 - Установить часы");
+    Console.WriteLine("3 - Установить минуты");
+    Console.WriteLine("4 - Установить секнды");
+    Console.WriteLine("5 - Выход");
+    Console.WriteLine();
+    Console.Write("Выберите действие: ");
+    t.Menu = byte.Parse(Console.ReadLine()!);
+
+    if (t.Menu == 1)
+    {
+        Console.Write("Введите часы: ");
+        t.Hours = byte.Parse(Console.ReadLine()!);
+        Console.Write("Введите минуты: ");
+        t.Minutes = byte.Parse(Console.ReadLine()!);
+        Console.Write("Введите секунды: ");
+        t.Secundes = byte.Parse(Console.ReadLine()!);
+    }
+    else if (t.Menu == 2)
+    {
+        Console.Write("Введите часы: ");
+        t.Hours = byte.Parse(Console.ReadLine()!);
+    }
+    else if (t.Menu == 3)
+    {
+        Console.Write("Введите минуты: ");
+        t.Minutes = byte.Parse(Console.ReadLine()!);
+    }
+    else if (t.Menu == 4)
+    {
+        Console.Write("Введите секунды: ");
+        t.Secundes = byte.Parse(Console.ReadLine()!);
+    }
+    else if (t.Menu == 5) Environment.Exit(0);
+    else
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Нет такого номера меню!!!");
+        Console.ForegroundColor = ConsoleColor.White;
+        continue;
+    }
+    Console.Clear();
+}
 
 
 
