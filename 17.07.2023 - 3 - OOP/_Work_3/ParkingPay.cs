5f26554e4576d6d33d77c4337921e7c9decf0775
AVcemik <aveal.cemik@gmail.com>
2023-07-21 15:25:54 +0200

    save

---
+++ 17.07.2023 - 3 - OOP/_Work_3/ParkingPay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Work_3
+{
+    internal class ParkingPay
+    {
+        public string Name { get; private set; }
+        public string Adress { get; private set; }
+        public int TotalSpot { get; set; }
+        public decimal Tariff { get; set; }
+
+        public ParkingPay(string name, string adress, int totalSpot, decimal tariff)
+        {
+            Name=name;
+            Adress=adress;
+            TotalSpot=totalSpot;
+            Tariff=tariff;
+        }
+
+
+    }
+}
