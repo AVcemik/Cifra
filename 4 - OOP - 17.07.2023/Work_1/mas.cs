a826254d8715d7d928fee06707e57d802f725063
cemik <aveal.cemik@gmail.com>
2023-07-23 18:27:59 +0200

    save

--- 4 - OOP - 17.07.2023/Work_1/mas.cs
+++ 4 - OOP - 17.07.2023/Work_1/mas.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Work_1
 {
-    internal class mas
+    internal class masiv
     {
+        private string[] Mas { get; set; }
+        public int Lenght { get; private set; }
+
+        masiv (int lenght)
+        {
+            Mas = new string[lenght];
+            Lenght = lenght;
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < Lenght; i++)
+            {
+                Console.WriteLine($"{i}: {Mas[i]}");
+            }
+        }
     }
 }
