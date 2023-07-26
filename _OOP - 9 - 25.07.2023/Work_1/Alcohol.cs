dcc1ee3078504ada3b812bb9feadc464643d3437
AVcemik <aveal.cemik@gmail.com>
2023-07-26 18:40:29 +0200

    save

---
+++ _OOP - 9 - 25.07.2023/Work_1/Alcohol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_1
+{
+    internal class Alcohol:Liquid
+    {
+        public double Strong { get; set; }
+        public new  void setDensity(double strong)
+        {
+            Strong = strong;
+        }
+    }
+}
---
+++ _OOP - 9 - 25.07.2023/Work_1/Liquid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_1
+{
+    internal class Liquid
+    {
+        public string? Name { get; set; }
+        public double Density { get; set; }
+        public void setDensity(double density)
+        {
+            Density = density;
+        }
+    }
+}
