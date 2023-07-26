c10f329aee7852c996ed0fa634b71b3a9f364d4d
AVcemik <aveal.cemik@gmail.com>
2023-07-26 09:20:00 +0200

    new HomeWork

---
+++ _OOP - 5 - 19.07.2023/Work_1/ParkingPay.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Work_3
+{
+    internal class ParkingPay
+    {
+        private string? name;
+        private string? adress;
+        private int totalSpot;
+        private decimal tariff;
+        private int occupiedSpot;
+        public string? Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public string? Adress
+        {
+            get { return adress; }
+            private set { adress = value; }
+        }
+        public int TotalSpot
+        {
+            get { return totalSpot; }
+            set { totalSpot = value; }
+        }
+        public decimal Tariff
+        {
+            get { return tariff; }
+            set { tariff = value; }
+        }
+        public int OccupiedSpot
+        {
+            get { return occupiedSpot; }
+            set { occupiedSpot = value; }
+        }
+
+        public ParkingPay(string name, string adress, int totalSpot, decimal tariff)
+        {
+            Name=name;
+            Adress=adress;
+            TotalSpot=totalSpot;
+            Tariff=tariff;
+            OccupiedSpot=0;
+        }
+        public void LeaveAuto(int carsLeave)
+        {
+            if (carsLeave < 0)
+            {
+                throw new ArgumentException("Число уехавших машин не может быть отрицательным.");
+            }
+            else if (carsLeave > OccupiedSpot)
+            {
+                throw new ArgumentException("Число уехавших машин не может превышать число занятых мест.");
+            }
+            else OccupiedSpot -= carsLeave;
+        }
+        public void AddAuto(int carsAdd)
+        {
+            if (carsAdd < 0)
+            {
+                throw new ArgumentException("Число прибывших машин не может быть отрицательным.");
+            }
+            else if (carsAdd > TotalSpot-OccupiedSpot)
+            {
+                throw new ArgumentException("Число прибывших машин не может превышать кол-во свободных мест.");
+            }
+            else OccupiedSpot += carsAdd;
+        }
+        public void CountSpot()
+        {
+            Console.WriteLine($"Кол-во свободных мест: {TotalSpot - OccupiedSpot}");
+        }
+    }
+}
---
+++ _OOP - 5 - 19.07.2023/Work_1/Program.cs
@@ -0,0 +1,63 @@
+﻿// 3.Создать класс «Платная автостоянка», включающий данные-элементы:
+// название, место расположения, количество мест, тариф. Функции-элементы:
+// •	создание и инициализация (конструктор),
+// •	количество уехавших машин (в аргументе),
+// •	количество машин, желающих встать на стоянку (в аргументе),
+// •	выдача сообщения о количестве свободных мест,
+// •	деструктор.
+
+using _Work_3;
+
+ParkingPay autoParking1 = new("Северная", "Калининград, ул. Вокзальная 43", 50, 60);
+
+while (true)
+{
+    Console.WriteLine("Меню");
+    Console.WriteLine("1 - Добавить автомобили");
+    Console.WriteLine("2 - Убрать автомобилей");
+    Console.WriteLine("3 - Кол-во свободных мест");
+    Console.WriteLine("0 - Выход");
+    Console.WriteLine();
+
+    if (autoParking1.OccupiedSpot < 50)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        autoParking1.CountSpot();
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    if (autoParking1.OccupiedSpot == 50)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        autoParking1.CountSpot();
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    Console.Write("\nВведите номер меню: ");
+    string number = Console.ReadLine()!;
+
+    if (number == "1")
+    {
+        Console.Write("Введите кол-во автомобилей: ");
+        autoParking1.AddAuto(int.Parse(Console.ReadLine()!));
+    }
+    else if (number == "2")
+    {
+        Console.Write("Введите кол-во убывших автомобилей: ");
+        autoParking1.LeaveAuto(int.Parse(Console.ReadLine()!));
+    }
+    else if (number == "3")
+    {
+        autoParking1.CountSpot();
+    }
+    else if (number == "0")
+    {
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("Некоректно введен номер!!!");
+    }
+    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+    Console.ReadKey();
+    Console.Clear();
+}
+
