8a87a995f7b5f5a2e9b489ced9a0e38665794f88
cemik <aveal.cemik@gmail.com>
2023-08-04 22:43:14 +0200

    ready

---
+++ _OOP - 4 - 17.07.2023/Work_1/Masiv.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_1
+{
+    internal class Masiv
+    {
+        private string[] mas;
+        private static int Length;
+
+        public Masiv(int lenght)
+        {
+            this.mas = new string[lenght];
+            Length=lenght;
+        }
+
+        public Masiv()
+        {
+            this.mas = new string[Length];
+        }
+        // Установка длины массивов
+        public static void SetLength(int length)
+        {
+            Length = length;
+        }
+        public int GetLength()
+        {
+            return Length;
+        }
+        
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Length)
+                    return mas[index];
+                else
+                    throw new IndexOutOfRangeException("Индекс находится за пределами массива.");
+            }
+            set
+            {
+                if (index >= 0 && index < Length)
+                    mas[index] = value;
+                else
+                    throw new IndexOutOfRangeException("Индекс находится за пределами массива.");
+            }
+        }
+        // Метод для выполнения операции поэлементного сцепления двух массивов
+        public void Concat(Masiv other)
+        {
+            if (Length != other.GetLength())
+                throw new ArgumentException("Массивы должны иметь одинаковую длину для выполнения сцепления.");
+
+            for (int i = 0; i < Length; i++)
+                mas[i] = mas[i] + other[i];
+        }
+
+        // Метод для выполнения операции слияния двух массивов с исключением повторяющихся элементов
+        public void MergeUnique(Masiv other)
+        {
+            if (Length != other.GetLength())
+                throw new ArgumentException("Массивы должны иметь одинаковую длину для выполнения слияния.");
+
+            for (int i = 0; i < Length; i++)
+                mas[i] = string.Join("", mas[i], other[i]).Distinct().Aggregate("", (current, next) => current + next);
+        }
+        // Вывод всего массива
+        public void Print(int number)
+        {
+            Console.WriteLine($"Массив: {number}");
+
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"{i}-{mas[i]} | ");
+            }
+            Console.WriteLine();
+        }
+        // Вывод элемента массива по индексу
+        public void PrintElement(int index)
+        {
+            Console.WriteLine($"{index}-{this[index]}");
+            Console.WriteLine();
+        }
+
+        // Заполнение массива
+        public void InputMasiv(int number)
+        {
+            Console.WriteLine($"Заполнение массива: {number}");
+
+            for (int i = 0; i < GetLength(); i++)
+            {
+                Console.Write($"Введите элемент номер {i+1}: ");
+                mas[i] = Console.ReadLine()!;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- _OOP - 4 - 17.07.2023/Work_1/Program.cs
+++ _OOP - 4 - 17.07.2023/Work_1/Program.cs
@@ -6,3 +6,39 @@
 // массивов с исключением повторяющихся элементов, а также вывод
 // на экран элемента массива по заданному индексу и всего массива.
 
+using Work_1;
+
+Console.Write("Введите длину массива: ");
+Masiv.SetLength(int.Parse(Console.ReadLine()!));
+
+Masiv mas1 = new();
+Masiv mas2 = new();
+Masiv mas3 = new();
+Masiv mas4 = new();
+
+mas1.InputMasiv(1);
+mas2.InputMasiv(2);
+
+mas1.Print(1);
+mas2.Print(2);
+
+Console.WriteLine();
+Console.WriteLine("Слияние двух массивов");
+mas3 = mas1;
+mas3.Concat(mas2);
+mas3.Print(3);
+
+Console.WriteLine();
+Console.WriteLine("Слияние двух массивов с исключением повторов");
+mas4 = mas1;
+mas4.MergeUnique(mas2);
+mas4.Print(4);
+
+Console.WriteLine();
+Console.Write("Введите номер элемента последнего массива для его вывода: ");
+mas4.PrintElement(int.Parse(Console.ReadLine()!));
+
+
+
+//Console.Write($"Введите {mas.Lenght} символов: ");
+//mas = char.Parse(Console.ReadLine());
