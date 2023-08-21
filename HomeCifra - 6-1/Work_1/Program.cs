5c8e3b67b7d5f48588079d292e09e41d7091d96a
Александр <aveal.cemik@gmail.com>
2023-08-21 18:55:11 +0200

    Ready

---
+++ HomeCifra - 6-1/Work_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашнее задание. Тема 6. Урок 1.
+// Преоброзование типов и способы
+
+// Есть исходный тип данных :
+// string, хранящий число 28
+// Конвентируйте его в следующие типы данных, последовательно:
+// сначало в byte,
+// затем из предыдущего в uint,
+// затем из предыдущего в int,
+// затем из предыдущего в long,
+// затем из предыдущего в double.
+
+string str = "28";
+
+uint _uint = uint.Parse(str);
+int _int = Convert.ToInt32(_uint);
+long _long = Convert.ToInt64(_int);
+double _double = Convert.ToDouble(_long);
+
+Console.WriteLine("string - " + str.GetType()!);
+Console.WriteLine("uint - " + _uint.GetType()!);
+Console.WriteLine("int - " + _int.GetType()!);
+Console.WriteLine("long - " + _long.GetType()!);
+Console.WriteLine("double - " + _double.GetType()!);
