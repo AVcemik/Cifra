4fa53a4dea25cbc3b5085d0cd2973a4fb3c13b19
AVcemik <aveal.cemik@gmail.com>
2023-08-31 13:18:05 +0200

    homeWork 32-1 - ready

---
+++ HomeCifra - 32-1/TreeConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿string defaultPath = @"C:\Users\Print01\Desktop";
+
+PrintDirectory(new DirectoryInfo(defaultPath), "", true);
+static void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+{
+    Console.Write(indent);
+    if (lastDirectory) { Console.Write("└─"); indent += " "; }
+    else { Console.Write("├─"); indent += "│ "; }
+    Console.WriteLine(directory.Name);
+
+    DirectoryInfo[] subDirectory = directory.GetDirectories();
+    for (int i = 0; i < subDirectory.Length; i++)
+    {
+        PrintDirectory(subDirectory[i], indent, i == subDirectory.Length - 1);
+    }
+}
