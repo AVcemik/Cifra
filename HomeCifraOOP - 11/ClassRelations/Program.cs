48e45b33b34978e841d4673b2c62e4b7c851f29f
cemik <aveal.cemik@gmail.com>
2023-09-21 22:45:52 +0200

    in process...

---
+++ HomeCifraOOP - 11/ClassRelations/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Домашняя работа
+    Задача на взаимодействие между классами. Разработать систему «Автобаза». 
+    Диспетчер распределяет заявки на Рейсы между Водителями и назначает для этого Автомобиль. 
+    Водитель может сделать заявку на ремонт. Диспетчер может отстранить Водителя от работы. 
+    Водитель делает отметку о выполнении Рейса и состоянии Автомобиля.
+ */
+
+
+
+class Application
+{
+    //private Trip 
+}
+class Dispatcher
+{
+    private string _name;
+}
+
+class Trip
+{
+    private string _name;
+    private bool _status;
+}
+
+class Driver
+{
+    private string _name;
+}
+
+class Car
+{
+    private string _mark;
+    private string _number;
+    private int _status;
+
+}
+
