85cfdc6be1eadf5612b8c350476376c210bd53c4
cemik <aveal.cemik@gmail.com>
2023-10-01 23:34:21 +0200

    Задача 2 и 3 готовы Остались задачи 6 и 7

--- HomeCifraOOP - 16-2/Task_2/Task_2.cs
+++ HomeCifraOOP - 16-2/Task_2/Task_2.cs
@@ -7,9 +7,46 @@
  */
 
 Dictionary<string, int> nameVisit = new Dictionary<string, int>();
-int count = 0;
+nameVisit.Add("Administrator", 0);
+
+string name = "";
+
 while (true)
 {
-    Console.Write("Введите имя: ");
+    int count = 1;
 
+    Console.Write("Введите имя: ");
+    name = Console.ReadLine();
+
+    if (name.ToLower() == "выход")
+    {
+        break;
+    }
+
+    foreach (KeyValuePair<string, int> item in nameVisit)
+    {
+        if (item.Key == name)
+        {
+            count = item.Value;
+            count++;
+            nameVisit.Remove(item.Key);
+            nameVisit.Add(name, count);
+            count = 1;
+            break;
+        }
+        else if (count == nameVisit.Count)
+        {
+            count = 1;
+            nameVisit.Add(name, count);
+            break;
+        }
+        count++;
+    }  
+}
+
+Console.WriteLine();
+
+foreach (KeyValuePair<string, int> item in nameVisit)
+{
+    Console.WriteLine($"Кол-во посещений у {item.Key}: {item.Value}");
 }
--- HomeCifraOOP - 16-2/Task_3/Task_3.cs
+++ HomeCifraOOP - 16-2/Task_3/Task_3.cs
@@ -1,7 +1,25 @@
 ﻿/*
-                Задача №3. Проверка на уникальность:
-Напишите функцию, которая принимает массив и возвращает true, если
-все элементы в массиве уникальны, и false в противном случае.
+            Задача №3. Проверка на уникальность:
+Напишите функцию, которая принимает массив и возвращает true, 
+если все элементы в массиве уникальны, и false в противном случае.
 Используйте HashSet для решения этой задачи.
  */
 
+int[] masivTrue = new int[] {1, 2, 3, 4, 5 };
+int[] masivFalse = new int[] { 1, 2, 2, 5, 5 };
+
+Console.WriteLine("masivTrue: " + CheckUnique(masivTrue));
+Console.WriteLine("masivFalse: " + CheckUnique(masivFalse));
+bool CheckUnique(int[] masiv)
+{
+    HashSet<int> hashSet = new HashSet<int>();
+
+    foreach (int item in masiv)
+    {
+        if (!hashSet.Add(item))
+        {
+            return false;
+        }
+    }
+    return true;
+}
