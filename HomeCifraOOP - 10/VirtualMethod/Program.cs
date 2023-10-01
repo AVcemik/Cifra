3829e3767a74f0d0b91c881556edf5d0ef2bfdaf
cemik <aveal.cemik@gmail.com>
2023-10-01 16:55:48 +0200

    Домашнее задание №10 виртуальные методы

--- HomeCifraOOP - 10/VirtualMethod/Program.cs
+++ HomeCifraOOP - 10/VirtualMethod/Program.cs
@@ -11,3 +11,58 @@
     представлении числа) производятся на основе значения, равного сумме значений полей индексируемого 
     объекта.
  */
+
+SecondBases number = new SecondBases(5);
+number[0] = 165489;
+number[1] = 597615;
+number[2] = 874135;
+number[3] = 913257;
+number[4] = 995229;
+
+Console.WriteLine(number[0, 3]);
+Console.WriteLine(number[1, 1]);
+Console.WriteLine(number[2, 5]);
+Console.WriteLine(number[3, 4]);
+Console.WriteLine(number[4, 6]);
+
+class Bases
+{
+    public int[] number;
+    public Bases(int index)
+    {
+        number = new int[index];
+    }
+    public virtual int this[int index]
+    {
+        get
+        {
+            return number[index];
+        }
+        set
+        {
+            number[index] = value;
+        }
+    }
+} 
+class SecondBases : Bases
+{
+    public SecondBases(int index) : base(index)
+    {
+    }
+    public int this[int index, int index2]
+    {
+        get
+        {
+            int temp = number[index];
+            for (int i = 1; i < index2; i++)
+            {
+                temp /= 10;
+            }
+            return temp % 10;
+        }
+        set
+        {
+
+        }
+    }
+}
