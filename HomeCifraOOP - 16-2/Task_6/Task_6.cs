ec0ae076fb02b869a5ca98d2177a020c030c259d
Александр <aveal.cemik@gmail.com>
2023-10-16 18:23:33 +0200

    Задача 6 решена Осталось задача 7

--- HomeCifraOOP - 16-2/Task_6/Task_6.cs
+++ HomeCifraOOP - 16-2/Task_6/Task_6.cs
@@ -6,8 +6,9 @@
  */
 
 
+// Не понял как через HashSet отслеживать проданные товары...
 
-Dictionary<string, int> item =  new Dictionary<string, int>();
+Dictionary<string, int> item = new Dictionary<string, int>();
 HashSet<string> sell = new HashSet<string>();
 string command;
 int count = 0;
@@ -19,10 +20,11 @@ DisplayMenu();
 void AddItemAndCountUp()
 {
     command = CommandInput("Введите название товара: ");
-
+    Console.Clear();
     if (item.ContainsKey(command.ToLower()))
     {
         ColorTextDisplay(((int)ColorText.red), "\nТакой товар в списке уже имеется!!!");
+        ContinueProgramm();
     }
     else
     {
@@ -36,10 +38,11 @@ void AddItemAndCountUp()
 void AddItemInList()
 {
     command = CommandInput("Введите название товара: ");
-
+    Console.Clear();
     if (item.ContainsKey(command.ToLower()))
     {
         ColorTextDisplay(((int)ColorText.red), "\nТакой товар в списке уже имеется!!!");
+        ContinueProgramm();
     }
     else
     {
@@ -51,38 +54,78 @@ void AddItemInList()
 void AddCountUpItem()
 {
     Console.Clear();
-    count = 1;
     foreach (var i in item)
     {
-        Console.WriteLine($"{count} - {i.Key} - {i.Value} шт");
-        count++;
+        Console.WriteLine($"{i.Key} - {i.Value} шт");
     }
-    int commandInt = int.Parse(CommandInput("Введите номер товара: "));
-    if (commandInt > 0 && commandInt < item.Count)
-    {
-        string nameIteme = item.Keys.ToString()!;
-        commandInt = Convert.ToInt32(item.Values);
-        item.Remove(nameIteme);
-        commandInt += int.Parse(CommandInput("Введите кол-во"));
-        item.Add(nameIteme, commandInt);
-    }
-    else
-    {
-        ColorTextDisplay(((int)ColorText.red), "\nВы ввели не число");
-        ContinueProgramm();
-        DisplayMenu();
-    }
-
+    command = CommandInput("Введите название товара: ");
+    Console.Clear();
     if (item.ContainsKey(command.ToLower()))
     {
-        ColorTextDisplay(((int)ColorText.red), "\nТакой товар в списке уже имеется!!!");
-    }
-    else
-    {
-        item.Add(command, 0);
+        int countItem = item[command];
+        countItem += int.Parse(CommandInput("Введите кол-во: "));
+        item[command] = countItem;
         ColorTextDisplay(((int)ColorText.green), "Товар успешно добавлен");
         ContinueProgramm();
     }
+    else
+    {
+        ColorTextDisplay(((int)ColorText.red), "\nТакого товара нет!!!");
+        ContinueProgramm();
+    }
+}
+void SellItem()
+{
+    Console.Clear();
+    foreach (var i in item)
+    {
+        Console.WriteLine($"{i.Key} - {i.Value} шт");
+    }
+    command = CommandInput("Введите название товара: ");
+    Console.Clear();
+    if (item.ContainsKey(command.ToLower()))
+    {
+        int countItem = item[command];
+        Console.Write("Введите кол-во: ");
+        int sellCount = int.Parse(Console.ReadLine()!);
+        if (countItem - sellCount >= 0)
+        {
+            item[command] = countItem - sellCount;
+            sell.Add(command);
+            ColorTextDisplay(((int)ColorText.green), "\nТовар успешно продан");
+            ContinueProgramm();
+        }
+        else
+        {
+            ColorTextDisplay(((int)ColorText.red), "\nТакого кол-во нет в наличии!!!");
+            ContinueProgramm();
+        }
+    }
+    else
+    {
+        ColorTextDisplay(((int)ColorText.red), "\nТакого товара нет!!!");
+        ContinueProgramm();
+    }
+}
+void ListItem()
+{
+    Console.Clear();
+    foreach (var i in item) 
+    {
+        Console.WriteLine(i.Key + " - " + i.Value);
+    }
+    ContinueProgramm();
+}
+void ExitProgramm()
+{
+    Console.Clear();
+    Console.WriteLine("Проданные товары: ");
+    foreach (var i in sell)
+    {
+        Console.WriteLine("     - " + i);
+    }
+    ContinueProgramm();
+    Environment.Exit(0);
 }
 void DisplayMenu()
 {
@@ -91,15 +134,34 @@ void DisplayMenu()
     Console.WriteLine("2 - Добавить товар в список");
     Console.WriteLine("3 - Пополнить товар");
     Console.WriteLine("4 - Продать товар");
+    Console.WriteLine("5 - Список товаров");
     Console.WriteLine("0 - Выход");
 
     command = CommandInput("Введите номер меню: ");
-    if (command == "1") AddItemAndCountUp();
-    else if (command == "2") AddItemInList();
-    else if (command == "3") AddCountUpItem();
-
+    switch (command)
+    {
+        case "1":
+            AddItemAndCountUp();
+            break;
+        case "2":
+            AddItemInList();
+            break;
+        case "3":
+            AddCountUpItem();
+            break;
+        case "4":
+            SellItem();
+            break;
+            case "5":
+            ListItem();
+            break;
+        case "0":
+            ExitProgramm();
+            break;
+        default:
+            break;
+    }
     DisplayMenu();
-
 }
 
 string CommandInput(string wordSelect)
@@ -107,7 +169,6 @@ string CommandInput(string wordSelect)
     Console.Write(wordSelect);
     return Console.ReadLine()!.ToLower();
 }
-
 void ColorTextDisplay(int color, string world)
 {
     if (color == 0)
