1fb6d8eb9704abb43269e88e44d3e1b4e9e14920
cem1k <aveal.cemik@gmail.com>
2023-10-18 14:31:36 +0300

    start HomeWork

---
+++ HomeCifraOOP - 19-2/Interface/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+public class Empoyee
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public Task Tasks { get; set; }
+
+}
+
+public class Task : ICloneable
+{
+    public string Titel { get; set; }
+    public Priority Prioritys { get; set; }
+    public DateTime DueData { get; set; }
+    
+}
+
+public enum Priority
+{
+    Hight,
+    Midlle,
+    Low
+}
