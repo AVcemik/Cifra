0a76ff8f8228c7d83c6c159863328ccaff305d1b
cemik <aveal.cemik@gmail.com>
2023-10-26 22:16:50 +0200

    доделать

---
+++ HomeCifraOOP - 20/HomeCifra_20/Program.cs
@@ -0,0 +1,55 @@
+﻿Animals<Animal> priut = new Animals<Animal>();
+
+priut.AddAnimal(new Animal { NickName = "Барсик", Breed = "Кот", AcceptanceDate = DateTime.Now, Age = 3 });
+priut.AddAnimal(new Animal { NickName = "Шарик", Breed = "Собака", AcceptanceDate = DateTime.Now, Age = 5 });
+
+List<Animal> animals = priut.GetAnimals();
+
+foreach (Animal animal in animals)
+{
+    Console.WriteLine($"Кличка: {animal.NickName}, Порода: {animal.Breed}, Дата поступления: {animal.AcceptanceDate}, Возраст: {animal.Age}");
+}
+
+Animal foundAnimal = priut.GetConcreteAnimal(1);
+if (foundAnimal != null)
+{
+    Console.WriteLine($"Найдено животное с кличкой {foundAnimal.NickName}");
+}
+else
+{
+    Console.WriteLine("Животное не найдено");
+}
+
+
+public class Animal : IListAnimal
+{
+    public int Id { get; set; }
+    public string NickName { get; set; }
+    public string Breed {  get; set; }
+    public DateTime AcceptanceDate {  get; set; }
+    public int Age { get; set; }
+}
+
+public interface IListAnimal
+{
+    public int Id { get; set; }
+}
+public class Animals<T> where T : IListAnimal
+{
+    private List<T> animals = new List<T>();
+
+    public void AddAnimal(T animal)
+    {
+        animals.Add(animal);
+    }
+
+    public List<T> GetAnimals()
+    {
+        return animals;
+    }
+
+    public T GetConcreteAnimal(int id)
+    {
+        return animals.Find(animal => animal.Id == id);
+    }
+}
