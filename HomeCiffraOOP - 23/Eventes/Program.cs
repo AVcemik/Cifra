091117cd8e9f8a7b209cc8f511ecd5c21c34e942
cemik <aveal.cemik@gmail.com>
2023-10-26 22:01:21 +0200

    готово

---
+++ HomeCiffraOOP - 23/Eventes/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+
+
+
+Keyboard keyboard = new Keyboard();
+keyboard.KeyPressed += OnKeyPressed;
+
+while (true)
+{
+    keyboard.SimulateKeyPress(Console.ReadKey().Key);
+}
+
+
+
+static void OnKeyPressed(object sender, KeyPressedEventArgs e)
+{
+    Console.WriteLine($" нажата клавиша - \"{e.Key}\"");
+}
+
+    class Keyboard
+{
+    public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+    public void SimulateKeyPress(ConsoleKey key)
+    {
+        char character = GetConsoleKey(key);
+        OnKeyPressed(new KeyPressedEventArgs(character));
+    }
+
+    private char GetConsoleKey(ConsoleKey key)
+    {
+        if (key >= ConsoleKey.A && key <= ConsoleKey.Z)
+        {
+            return (char)('A' + (key - ConsoleKey.A));
+        }
+        else if (key >= ConsoleKey.D0 && key <= ConsoleKey.D9)
+        {
+            return (char)('0' + (ConsoleKey.D0));
+        }
+        else
+        {
+            return '\0';
+        }
+    }
+
+    protected virtual void OnKeyPressed(KeyPressedEventArgs e)
+    {
+        KeyPressed?.Invoke(this, e);
+    }
+}
+
+class KeyPressedEventArgs : EventArgs
+{
+    public char Key { get; }
+
+    public KeyPressedEventArgs(char key)
+    {
+        Key = key;
+    }
+}
