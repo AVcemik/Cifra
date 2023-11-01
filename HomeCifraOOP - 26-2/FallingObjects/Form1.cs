a5c646a4f51c26507b931bb543467e447f99fac3
Александр <aveal.cemik@gmail.com>
2023-11-01 18:40:17 +0200

    fix поток

--- HomeCifraOOP - 26-2/FallingObjects/Form1.cs
+++ HomeCifraOOP - 26-2/FallingObjects/Form1.cs
@@ -9,7 +9,7 @@ namespace FallingObjects
         public Form1()
         {
             InitializeComponent();
-            this.Controls.Add(label2);
+            Controls.Add(label2);
         }
 
         private void DownStone()
@@ -25,20 +25,18 @@ namespace FallingObjects
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Thread th = new Thread(() =>
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    int y = i;
-                    y += 10;
-                    lock (_lockJbject)
-                    {
-                        label2.Location = new Point(50, y);
-                    }
-                    Thread.Sleep(200);
-                }
-            });
+            Thread th = new Thread(ReplaseControl);
             th.Start();
+        }
+        public void ReplaseControl()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int y = i;
+                y += 20;
+                label2.Location = new Point(50, y);
+                Thread.Sleep(200);
+            }
         }
     }
 }
