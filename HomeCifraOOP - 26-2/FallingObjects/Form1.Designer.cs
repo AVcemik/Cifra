0ba59788829237ee82a1fde0b2f47a72e93de287
cem1k <aveal.cemik@gmail.com>
2023-11-01 10:40:53 +0300

    error

--- HomeCifraOOP - 26-2/FallingObjects/Form1.Designer.cs
+++ HomeCifraOOP - 26-2/FallingObjects/Form1.Designer.cs
@@ -28,7 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label2 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(275, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 0;
+            label2.Text = "asdasd";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(608, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 49);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
@@ -36,12 +57,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
             Name = "Form1";
             Text = "Падающие обекты";
             FormClosing += Form1_FormClosing;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label2;
+        private Button button1;
     }
 }
--- HomeCifraOOP - 26-2/FallingObjects/Form1.cs
+++ HomeCifraOOP - 26-2/FallingObjects/Form1.cs
@@ -4,57 +4,41 @@ namespace FallingObjects
 {
     public partial class Form1 : Form
     {
-        private List<Stone> stone = new List<Stone>();
-        private Random random = new Random();
-
-        PictureBox pictureBox1 = new();
-        PictureBox pictureBox2 = new();
-        PictureBox pictureBox3 = new();
-        private string _imagePath = Directory.GetCurrentDirectory() + "\\stone.png";
+        private static readonly object _lockJbject = new object();
+        //private string _imagePath = Directory.GetCurrentDirectory() + "\\stone.png";
         public Form1()
         {
             InitializeComponent();
-            this.Size = new Size(1000, 600);
-            pictureBox1 = SetSettingsStone();
-            pictureBox2 = SetSettingsStone();
-            pictureBox3 = SetSettingsStone();
-            Controls.Add(pictureBox1);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox3);
-
-
-
-            Thread thread_1 = new Thread(() =>
-            {
-                pictureBox1 = SetSettingsStone();
-                DownStone();
-            });
-
-            thread_1.Start();
-
+            this.Controls.Add(label2);
         }
+
         private void DownStone()
         {
-            for (int i = 0; i < 600; i++)
-            {
-                pictureBox1.Top += i;
-            }
+            
         }
-        private PictureBox SetSettingsStone()
-        {
-            Bitmap image = new Bitmap(_imagePath);
-            PictureBox pictureBox = new();
-            int x = random.Next(970);
-            pictureBox.Location = new Point(x, 0);
-            pictureBox.Size = new Size(30, 30);
-            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox.Image = image;
-            return pictureBox;
-        }
+
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int y = i;
+                    y += 10;
+                    lock (_lockJbject)
+                    {
+                        label2.Location = new Point(50, y);
+                    }
+                    Thread.Sleep(200);
+                }
+            });
+            th.Start();
         }
     }
 }
