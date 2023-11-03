d96db77f7e4850625035408f6e0919f6fc36fa2c
cemik <aveal.cemik@gmail.com>
2023-11-03 22:49:19 +0200

    new HW ready

---
+++ HomeCifraOOP - 26-6/Asynchron/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Asynchron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial(5);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Секунда " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        public static async void Factorial(int number)
+        {
+            int result = 1;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    result *= i;
+                    Task.Delay(250).Wait();
+                }
+            });
+            
+            Console.WriteLine($"\t\t\tФакториал числа {number} = {result}");
+        }
+    }
+}
