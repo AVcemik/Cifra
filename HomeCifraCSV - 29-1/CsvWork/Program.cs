24b70820258d30bbc3ead860e26003a7adc2bf27
cemik <aveal.cemik@gmail.com>
2023-11-28 17:11:34 +0200

    new homeWork 29-1 - Ready

---
+++ HomeCifraCSV - 29-1/CsvWork/Program.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvWork;
+using System.Globalization;
+
+StreamWriter streamWriter = new("Output.csv");
+
+CsvWriter csvWriter = new(streamWriter, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+List<Person> records = new()
+{
+    new Person {Name = "Александр", Age = 36},
+    new Person {Name = "Елена", Age = 25},
+};
+
+csvWriter.WriteRecords(records);
+
+streamWriter.Close();
+
+records.Clear();
+
+StreamReader streamReader = new("Output.csv");
+
+CsvReader csvReader = new(streamReader, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+records = csvReader.GetRecords<Person>().ToList();
+
+streamReader.Close();
+
+foreach (Person person in records)
+{
+    Console.WriteLine("{0} - {1}", person.Name, person.Age);
+}
