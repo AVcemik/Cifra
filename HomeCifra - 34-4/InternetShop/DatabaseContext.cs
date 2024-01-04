73610a91b9b57695f12f81d9f5a4fa18b1c15e2a
cemik <aveal.cemik@gmail.com>
2024-01-04 19:32:33 +0200

    ДЗ 34-4 Не решена запись сразу в два файла

---
+++ HomeCifra - 34-4/InternetShop/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InternetShop
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> option) : base(option)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ HomeCifra - 34-4/InternetShop/InternetShop.cs
@@ -0,0 +1,41 @@
+﻿using InternetShop;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+
+ConfigurationBuilder builder = new();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("ConnectionString.json");
+
+var config = builder.Build();
+string? connectionString = config.GetConnectionString("DefaultConnection");
+
+DbContextOptionsBuilder<DatabaseContext> optionsBuilder = new();
+MySqlServerVersion vers = new(new Version(8, 0, 25));
+
+
+StreamWriter logError = WriterLog("error.log", LogLevel.Error);
+StreamWriter logInfo = WriterLog("log.log", LogLevel.Information);
+StreamWriter logDebug = WriterLog("debug.log", LogLevel.Debug); // Записываает только этот файл, так как он последний!!! не смог решить проблему что бы записываалось все
+
+
+DbContextOptions<DatabaseContext> options = optionsBuilder.UseMySql(connectionString, vers).Options;
+
+
+using (DatabaseContext context = new(options))
+{
+
+}
+
+logError.Close();
+logInfo.Close();
+logDebug.Close();
+
+StreamWriter WriterLog(string path, LogLevel level)
+{
+    StreamWriter writer = new(path, true);
+    optionsBuilder.LogTo(writer.WriteLine, level);
+    return writer;
+}
