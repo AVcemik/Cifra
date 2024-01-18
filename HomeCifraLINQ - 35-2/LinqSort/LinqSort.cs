ca7d2a7a8bbded203f516f5d768bf40db3aa618b
cem1k <aveal.cemik@gmail.com>
2024-01-18 14:11:15 +0300

    HomeWork 35-2 - Finish

---
+++ HomeCifraLINQ - 35-2/LinqSort/LinqSort.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using LinqSort;
+using System.Globalization;
+
+string fullPath = Directory.GetCurrentDirectory() + "\\Movies.csv";
+List<Movie> movies = new();
+
+using (StreamReader reader = new(fullPath))
+{
+    CsvReader csvReader = new(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+    movies = csvReader.GetRecords<Movie>().ToList();
+}
+
+var result = movies.OrderByDescending(m => m.Year);
+
+foreach (var movie in result)
+{
+    Console.WriteLine($"номер: {movie.Id}\t Дата: {movie.Year}\t Жанр:{movie.Genre}\t Оценка: {movie.Graduate}\t Название: {movie.Name}");
+}
+
---
+++ HomeCifraLINQ - 35-2/LinqSort/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqSort
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Year { get; set; }
+        public string? Director { get; set; }
+        public string? Genre { get; set; }
+        public double Graduate { get; set; }
+        public Movie () { }
+    }
+}
