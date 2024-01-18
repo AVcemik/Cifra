efeb53933bc3bba6431d5fc29ea7adcd7ef2795f
cem1k <aveal.cemik@gmail.com>
2024-01-18 13:22:51 +0300

    HomeWork 35-1 - Finish

---
+++ HomeCifraLINQ - 35-1/LinqCsv/Good.cs
@@ -0,0 +1,12 @@
+﻿namespace LinqCsv
+{
+    public class Good
+    {
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public Good() { }
+
+    }
+}
---
+++ HomeCifraLINQ - 35-1/LinqCsv/Program.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using LinqCsv;
+using System.Globalization;
+
+string fullPath = Directory.GetCurrentDirectory() + "\\inventoryControl.csv";
+List<Good> listItem = new();
+
+using (StreamReader reader = new StreamReader(fullPath))
+{
+    CsvReader csvReader = new(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+    listItem = csvReader.GetRecords<Good>().ToList();
+}
+
+var result = listItem.Where(i => i.Quantity % 2 == 0);
+
+foreach (Good item in result)
+{
+    
+    Console.WriteLine($"{item.Name} - {item.Quantity} - {item.Price} - {item.Description}");
+} 
