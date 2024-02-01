496826d5ee748259e48f427f526f9cb05495fbdb
cemik <aveal.cemik@gmail.com>
2024-02-01 23:04:47 +0200

    Доделать CheckBox 86 ДЗ

---
+++ HomeCifraWPF - 86/HomeCifraWPF - 86/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HomeCifraWPF___86
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
